fe6ee498bd95c1d14ee95a9bc7681fa577c4a054
Eddy Yoel Fresno <goku990202@gmail.com>
2019-06-22 21:11:55 +0200

    #Version 1.03(Alpha3) Cambios visuales en el Main Activity ahoora presenta botones
     ##Se eliminan los elementos de toolbar del main y se adiere una interfaz con botones para escoger la activity en el espacio donde antes estaban los elementos sitio uci y el anno 2019

--- app/src/main/java/com/relme33/fiai/uci/relme33/MainActivity.java
+++ app/src/main/java/com/relme33/fiai/uci/relme33/MainActivity.java
@@ -22,75 +22,62 @@ import static android.Manifest.permission.CALL_PHONE;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
-        return true;
-    }
-    public boolean onOptionsItemSelected(MenuItem menuItem){
-        switch (menuItem.getItemId()){
-            case R.id.RELME_33:
-                Intent intent1 = new Intent(getApplicationContext(),RelmeInfoActivity.class);
-                startActivity(intent1);
-                finish();
-                break;
-            case R.id.Conferencistas:
-                Intent intent2 = new Intent(getApplicationContext(),ConferencistasActivity.class);
-                startActivity(intent2);
-                finish();
-                break;
-            case R.id.Modalidades:
-                Intent intent3 = new Intent(getApplicationContext(),ModalidadesActivity.class);
-                startActivity(intent3);
-                finish();
-                break;
-            case R.id.Programa:
-                Intent intent4 = new Intent(getApplicationContext(),ProgramaActivity.class);
-                startActivity(intent4);
-                finish();
-                break;
-            case R.id.Mapa:
-                showDialogWait();
-                Intent intent7 = new Intent(getApplicationContext(),MapaActivity.class);
-                startActivity(intent7);
-                finish();
-                break;
-            case R.id.Contactos:
-                Intent intent5 = new Intent(getApplicationContext(),ContactosActivity.class);
-                startActivity(intent5);
-                finish();
-                break;
-            case R.id.Acerca_de:
-                Intent intent6 = new Intent(getApplicationContext(),AcercaDeActivity.class);
-                startActivity(intent6);
-                finish();
-                break;
-        }
-        return true;
+
     }
 
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.link_uci_cu:
-                startActivity(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse(getString(R.string.sitio_uci))));
-                break;
             case R.id.relme_cartel_principal:
-                Intent intent3 = new Intent(getApplicationContext(),ModalidadesActivity.class);
+                Intent inten = new Intent(getApplicationContext(),ModalidadesActivity.class);
+                startActivity(inten);
+                finish();
+                break;
+            case R.id.menu_programa_eventos:
+                Intent intent1 = new Intent(getApplicationContext(),ProgramaActivity.class);
+                startActivity(intent1);
+                finish();
+                break;
+            case R.id.menu_relme:
+                Intent intent2 = new Intent(getApplicationContext(),RelmeInfoActivity.class);
+                startActivity(intent2);
+                finish();
+                break;
+            case R.id.menu_conferencistas:
+                Intent intent3 = new Intent(getApplicationContext(),ConferencistasActivity.class);
                 startActivity(intent3);
                 finish();
                 break;
+            case R.id.menu_modalidades:
+                Intent intent4 = new Intent(getApplicationContext(),ModalidadesActivity.class);
+                startActivity(intent4);
+                finish();
+                break;
+            case R.id.menu_mapa:
+                showDialogWait();
+                Intent intent5 = new Intent(getApplicationContext(),MapaActivity.class);
+                startActivity(intent5);
+                finish();
+                break;
+            case R.id.menu_contactos:
+                Intent intent6 = new Intent(getApplicationContext(),ContactosActivity.class);
+                startActivity(intent6);
+                finish();
+                break;
+            case R.id.menu_acerca:
+                Intent intent7 = new Intent(getApplicationContext(),AcercaDeActivity.class);
+                startActivity(intent7);
+                finish();
+                break;
+
+
         }
     }
-
     private void showDialogWait(){
         LayoutInflater inflater = LayoutInflater.from(this);
         View subView = inflater.inflate(R.layout.layout_promptdialog__map_charging, null);
